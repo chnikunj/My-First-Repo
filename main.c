2330b5c88324a87beff6e1ed559353f72eb9250c
Nikunj Chaudhary <nchaudhary02@qub.ac.uk>
2026-09-25 13:30:06 +0100

    c program

---
+++ main.c
@@ -0,0 +1,11 @@
+// This is a simple C program that prints "Hello, World!" to the console. It includes the standard input-output header file `<stdio.h>` which provides the `printf` function used for output. The `main` function is the entry point of the program, and it returns 0 to indicate successful execution.
+
+#include <stdio.h>
+
+int main()
+{
+    printf("Hello, World!\n");
+    return 0;
+}
+
+
---
+++ program.c
@@ -0,0 +1,12 @@
+
+#include <stdio.h>
+
+int main()
+{
+int a,b,c; /* variables declared as type int */
+    a = 3; /* assign value 3 to variable a */
+    b = 2; /* assign value 2 to variable b */
+    c = a+b; /* assign result of expression a+b */
+printf("%d + %d = %d\n",a,b,c);
+    
+}
